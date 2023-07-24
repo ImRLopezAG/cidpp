39ddefa12f6013f3dd7be4a5fcafe52ad71dfdb9
Angel Gabriel Lopez <88790737+ImRLopezAG@users.noreply.github.com>
2023-07-24 12:51:36 -0700

    Update contact.tsx

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,4 +1,33 @@
+import { useState } from 'react'
+
+interface IForm {
+  name: string
+  email: string
+  message: string
+}
 export const Contact = (): JSX.Element => {
+  const [form, setForm] = useState<IForm>({
+    name: '',
+    email: '',
+    message: ''
+  })
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    setForm({
+      name: '',
+      email: '',
+      message: ''
+    })
+  }
   return (
     <section className='flex flex-col justify-center [&>h1]:items-center'>
       <h1 className='text-4xl font-bold text-[#0071bc] mb-3'>Contáctanos</h1>
@@ -12,7 +41,10 @@ export const Contact = (): JSX.Element => {
       <h2 className='text-2xl font-bold text-[#0071bc] mb-3'>
         Obtenga información sobre nuestros servicios
       </h2>
-      <form className='flex flex-col gap-4 items-center mb-5'>
+      <form
+        className='flex flex-col gap-4 items-center mb-5'
+        onSubmit={handleSubmit}
+      >
         <div className='flex w-full gap-3 items-start'>
           <div className='flex flex-col w-1/2 justify-center gap-2 items-start'>
             <label htmlFor='name' className='text-xl'>
@@ -21,6 +53,8 @@ export const Contact = (): JSX.Element => {
             <input
               type='text'
               name='name'
+              onChange={handleChange}
+              value={form.name}
               id='name'
               className='border-2 w-full border-[#0071bc] rounded-md p-1'
             />
@@ -32,6 +66,8 @@ export const Contact = (): JSX.Element => {
             <input
               type='email'
               name='email'
+              onChange={handleChange}
+              value={form.email}
               id='email'
               className='border-2 w-full border-[#0071bc] rounded-md p-1'
             />
@@ -44,6 +80,8 @@ export const Contact = (): JSX.Element => {
           <textarea
             name='message'
             id='message'
+            onChange={handleChange}
+            value={form.message}
             className='border-2 w-full border-[#0071bc] rounded-md p-1'
             rows={5}
           />
